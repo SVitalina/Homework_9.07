bdfeebc944188b13988de6d9845b4e6d7f927c19
Oleksii <zankexd@icloud.com>
2022-07-06 20:33:01 +0200

    Task 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+int N = new Random().Next(1, 99);
+int N1 = 1;
+int Num = 0;
+
+Console.WriteLine( N );
+Console.WriteLine();
+
+while (N1<=N)
+{
+    Num = N1*N1*N1;
+    Console.WriteLine(Num);
+    N1++;
+}
